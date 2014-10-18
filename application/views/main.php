fbe2c4aceb4045a5ac750db6586d5cafccb5b776
williecheong <cheongwillie@hotmail.com>
2014-10-18 16:00:21 -0700

    font awesome problems

--- application/views/main.php
+++ application/views/main.php
@@ -143,7 +143,7 @@
         </div>
         <div class="text-center" id="fineprint">
             <span class="lead">
-                <span><i class="fa fa-cubes"></i></span>
+                <i class="fa fa-cubes"></i>
                 <?=APP_NAME?> 
                 <?=date('Y')?>
             </span>
