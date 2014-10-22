077491d9acf97e1c5f22c133d8353b3f5144e1a8
williecheong <cheongwillie@hotmail.com>
2014-10-21 23:27:00 -0700

    some extras

--- assets/js/main_logged_out.js
+++ assets/js/main_logged_out.js
@@ -3,8 +3,8 @@ var app = angular.module('myApp', ['ui.bootstrap']);
 app.controller('myController', function( $scope, $sce, $http, $filter ) {
     $scope.tooltipContent = function() {
         var html = '';
-            html += '<span class="lead text-left text-muted">';
-            html +=     'Inspired by: ';
+            html += '<span class="lead text-muted">';
+            html +=     'Experiment inspired by: ';
             html +=     '<a href="//twitch.tv/twitchplayspokemon" target="_blank">';
             html +=         '<i class="fa fa-twitch fa-lg"></i>';
             html +=     '</a>';
